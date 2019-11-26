e739abdb00c5a9e70dd42e7cc1be6dba4ec28561
Melissa Castorena <mcastorena0316@gmail.com>
2019-11-26 11:19:03 -0600

    adding new players section

--- bin/main.rb
+++ bin/main.rb
@@ -1,19 +1,35 @@
 #!/usr/bin/env ruby
 puts "Hello World!!"
 
-=begin
+
 class UserInput
 
-  def initialize(name)
-    @name = name
-  end
-=end
-
-  def introduction
+  def initialize()
+  # def introduction
+  begin 
     puts "You are about to start the Tic Tac Toe game."
     puts "What's the name of the first player?"
-    name = gets.chomp
+    @player1 = gets.chomp
+    puts "What's the name of the second player?"
+    @player2= gets.chomp
+  # end
+    end 
   end
+end
+prueba= UserInput.new
 
 
+#ESTABLISH X OR O FOR EACH USER
 
+# CREATION OF BOARD
+# board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+# 3.times do |x|
+# p " #{board[(3*x)]}  #{board[(3*x)+1]}  #{board[(3*x)+2]}"
+
+# end
+
+
+#ASK PLAYER FOR A MOVE 
+
+
+#UPDATE THE BOARD
