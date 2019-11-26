87bf6f438868c0ec7c643e30aeb47c03887e987c
Melissa Castorena <mcastorena0316@gmail.com>
2019-11-26 12:49:33 -0600

    finishing inputs for the game

--- bin/main.rb
+++ bin/main.rb
@@ -6,14 +6,16 @@ class UserInput
 
   def initialize
 
-    #puts "You are about to start the Tic Tac Toe game."
-    #puts "What's the name of the first player?"
-    #@player1 = gets.chomp
-    #puts "What's the name of the second player?"
-    #@player2 = gets.chomp
+    puts "You are about to start the Tic Tac Toe game."
+    puts "What's the name of the first player?"
+    @player1 = gets.chomp
+    puts "What's the name of the second player?"
+    @player2 = gets.chomp
 
     @board = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
     @turn = true
+    @play =true
+    @token
   end
 
   def creation_of_board
@@ -23,39 +25,49 @@ class UserInput
   end
 
   def game
-    creation_of_board
-    move
-
+      counter= 0
+      creation_of_board
+    while @play
+      @turn ? @token = "X": @token = "O"
+      move
+      @play = counter < 8
+      counter += 1
+      p counter
+      @turn= !@turn
+    end
   end
+
   def player_symbol
-    puts "Choose a symbol:\n 1.- X\n 2.- O"
+    puts "{ @turn ? @player1 : @player2 },Choose a symbol:\n 1.- X\n 2.- O"
     symbol_1 = gets.chomp
     symbol_2 = gets.chomp
     p symbol_1
     p symbol_2
   end
+
   def move
-    puts "Choose a number of the board: "
+    puts "#{ @turn ? @player1 : @player2 } Choose a number of the board: "
     number = gets.chomp
-    if number > "9" && number < "1"
-      puts "Error, you need to choose another number for moves."
+     number=number.to_i
+    if number > 9 || number < 1
+      p "Error, you need to choose another number for moves."
     else
-      @board.split(",").map {|s|s.to_i}
       case number
-      when number == 1 || number == 2 || number == 3
-        puts @board[0][number - 1]
-        else
+      when 1..3
+         @board[0][number - 1] = @token
+
+         creation_of_board()
+      when 4..6
+        puts @board[1][number - 4] = @token
+        creation_of_board()
+
+      else
+        puts @board[2][number - 7] = @token
+        creation_of_board()
       end
     end
-
   end
 end
 prueba = UserInput.new
 prueba.game
-#ESTABLISH X OR O FOR EACH USER
 
-
-#ASK PLAYER FOR A MOVE 
-
-
-#UPDATE THE BOARD
