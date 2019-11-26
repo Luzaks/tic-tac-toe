248f52f2c27ab0ec8a57ceae5185d824fd663ebf
Luzaks <kanemekanik@gmail.com>
2019-11-26 10:30:17 -0600

    Trying out the code in the main.rb file, code is not displaying

--- bin/main.rb
+++ bin/main.rb
@@ -1,2 +1,19 @@
 #!/usr/bin/env ruby
 puts "Hello World!!"
+
+=begin
+class UserInput
+
+  def initialize(name)
+    @name = name
+  end
+=end
+
+  def introduction
+    puts "You are about to start the Tic Tac Toe game."
+    puts "What's the name of the first player?"
+    name = gets.chomp
+  end
+
+
+
