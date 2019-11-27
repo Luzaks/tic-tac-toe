d8eb53f983e40db3748f57e56bb7d32062d59cd6
Luzaks <kanemekanik@gmail.com>
2019-11-27 12:14:20 -0600

    Fixing rubocop offenses

--- bin/main.rb
+++ bin/main.rb
@@ -1,5 +1,5 @@
 #!/usr/bin/env ruby
-
+# frozen_string_literal: true.
 puts 'Hello World!!'
 
 class UserInput #:nodoc:
@@ -14,8 +14,8 @@ class UserInput #:nodoc:
     @turn = true
     @play = true
     @token = nil
-    @number= nil
-    @array_of_number= []
+    @number = nil
+    @array_of_number = []
   end
 
   def creation_of_board
@@ -28,48 +28,44 @@ class UserInput #:nodoc:
     creation_of_board
     while @play
       @token = @turn ? 'X' : 'O'
-      move #method for adding an x or and o to the board 
-      # compares_move   method for comparing if its a winning move, draw move, invalid, or continue playing- ITS GOING TO BE ADDED TO THE LOGIC
+      move # method for making valid moves and informs to the user "if" the move is invalid.
+      # compares_move method for comparing "if" its a winning move, draw move or continue playing- ITS GOING TO BE ADDED TO THE LOGIC
       @turn = !@turn
-      #  def end_game this is going to determine if there is a winning move, or a tie move, the program end- ITS GOING TO BE ADDED TO THE LOGIC
+      #  method end_game will "end" the game and display who wins or "if" its a tie and goes out from the loop- ITS GOING TO BE ADDED TO THE LOGIC
     end
-    #def display who wins or if its a tie
   end
-
 
   def move
     puts "#{@turn ? @player1 : @player2} Choose a number of the board: "
     @number = gets.chomp
     @number = @number.to_i
-      until  @number < 10 && @number > 0 && @array_of_number.none?(@number)
+    until @number < 10 && @number.positive? && @array_of_number.none?(@number)
       p @array_of_number.none?(@number)
-        puts 'Please choose another location: '
-        @number = gets.chomp
-        @number = @number.to_i
-      end 
+      puts 'Please choose another location: '
+      @number = gets.chomp
+      @number = @number.to_i
+    end
     cases_for_number_selected
-    p @array_of_number 
+    p @array_of_number
     creation_of_board
   end
-     
-      def cases_for_number_selected
-        case @number
-         when 1..3
-             @board[0][@number - 1] = @token
-             @array_of_number << @number
-         when 4..6
-           puts @board[1][@number - 4] = @token
-           @array_of_number << @number
-         when 7..9
-           puts @board[2][@number - 7] = @token
-           @array_of_number << @number
-         else
-           puts "Estoy en el else"
-          end
-        end
 
+  def cases_for_number_selected
+    case @number
+    when 1..3
+      @board[0][@number - 1] = @token
+      @array_of_number << @number
+    when 4..6
+      puts @board[1][@number - 4] = @token
+      @array_of_number << @number
+    when 7..9
+      puts @board[2][@number - 7] = @token
+      @array_of_number << @number
+    else
+      puts 'Estoy en el else'
+    end
+  end
 end
-
 
 prueba = UserInput.new
 prueba.game
