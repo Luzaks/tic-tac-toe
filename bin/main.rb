147850a34f699320f902d78b8e14b0ec55f00f20
Luzaks <kanemekanik@gmail.com>
2019-11-28 14:19:02 -0600

    Method for the tie game added, logic while working properly

--- bin/main.rb
+++ bin/main.rb
@@ -4,7 +4,6 @@ puts 'Hello World!!'
 require_relative "../lib/logic"
 
 class UserInput #:nodoc:
-
   def initialize
     puts 'You are about to start the Tic Tac Toe game.'
     puts "What's the name of the first player?"
@@ -19,32 +18,32 @@ class UserInput #:nodoc:
     @logic.creation_of_board
     while @play
       @logic.change_turn
-      move 
-      if  @logic.winning_moves == true
-      puts "#{@logic.turn ? @player1 : @player2}  ya ganaste "
-      @play=false
-      else 
-      @logic.turn = !@logic.turn
+      move
+      if @logic.winning_moves
+        puts "#{@logic.turn ? @player1 : @player2}  ya ganaste "
+        @play = false
+      elsif @logic.tie != false
+        puts "Es un empate"
+        @play = false
+      else
+        @logic.turn = !@logic.turn
       end
       #  method end_game will "end" the game and display who wins or "if" its a tie and goes out from the loop- ITS GOING TO BE ADDED TO THE LOGIC
     end
   end
-
-
-   
 
   def move
     puts "#{@logic.turn ? @player1 : @player2} Choose a number of the board: "
     @logic.number = gets.chomp
     @logic.number = @logic.number.to_i
     until @logic.number < 10 && @logic.number.positive? && @logic.array_of_number.none?(@logic.number)
-       @logic.array_of_number.none?(@logic.number)
+      @logic.array_of_number.none?(@logic.number)
       puts 'Please choose another location: '
       @logic.number = gets.chomp
       @logic.number = @logic.number.to_i
     end
     @logic.cases_for_number_selected
-     @logic.array_of_number
+    @logic.array_of_number
     @logic.creation_of_board
   end
 end
--- lib/logic.rb
+++ lib/logic.rb
@@ -1,61 +1,76 @@
 # frozen_string_literal: true
 
-class  GameLogic
-  attr_accessor :board, :array_of_number, :number, :token, :turn
+class GameLogic
+  attr_accessor :board, :array_of_number, :number, :token, :turn, :tie
   def initialize
-    @board = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+    @@board = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
     @array_of_number = []
     @number = nil
-    @token = nil
+    @@token = nil
     @turn = true
+    @@tie = nil
   end
 
   def creation_of_board
     3.times do |x|
-      p @board[x].join(' ')
+      p @@board[x].join(' ')
     end
   end
 
   def change_turn
-    @token = @turn ? 'X' : 'O'
+    @@token = @turn ? 'X' : 'O'
   end
 
   def cases_for_number_selected
     case @number
     when 1..3
-      @board[0][@number - 1] = @token
+      @@board[0][@number - 1] = @@token
       @array_of_number << @number
     when 4..6
-      puts @board[1][@number - 4] = @token
+      puts @@board[1][@number - 4] = @@token
       @array_of_number << @number
     when 7..9
-      puts @board[2][@number - 7] = @token
+      puts @@board[2][@number - 7] = @@token
       @array_of_number << @number
     else
       puts 'Estoy en el else'
     end
   end
 
-  def winning_moves 
-    case 
-      when @board[0][0] == @token &&  @board[0][1] == @token && @board[0][2] == @token
-       return true
-      when @board[1][0] == @token &&  @board[1][1] == @token && @board[1][2] == @token
-        return true
-      when @board[2][0] == @token &&  @board[2][1] == @token && @board[2][2] == @token
-        return true
-      when @board[0][0] == @token && @board[1][0] == @token && @board[2][0] == @token
-        return true
-      when @board[0][1] == @token &&  @board[1][1] == @token &&  @board[2][1] == @token 
-        return true
-      when  @board[0][2] == @token && @board[1][2] == @token && @board[2][2] == @token
-        return true
-      when  @board[0][0] == @token && @board[1][1] == @token && @board[2][2] == @token
-        return true
-      when  @board[0][2] == @token && @board[1][1] == @token && @board[2][0] == @token
-        return true
-      else 
-        return false
+  def winning_moves
+    case
+    when @@board[0][0] == @@token &&  @@board[0][1] == @@token && @@board[0][2] == @@token
+      true
+    when @@board[1][0] == @@token &&  @@board[1][1] == @@token && @@board[1][2] == @@token
+      true
+    when @@board[2][0] == @@token &&  @@board[2][1] == @@token && @@board[2][2] == @@token
+      true
+    when @@board[0][0] == @@token && @@board[1][0] == @@token && @@board[2][0] == @@token
+      true
+    when @@board[0][1] == @@token &&  @@board[1][1] == @@token && @@board[2][1] == @@token
+      true
+    when  @@board[0][2] == @@token && @@board[1][2] == @@token && @@board[2][2] == @@token
+      true
+    when  @@board[0][0] == @@token && @@board[1][1] == @@token && @@board[2][2] == @@token
+      true
+    when  @@board[0][2] == @@token && @@board[1][1] == @@token && @@board[2][0] == @@token
+      true
+    else
+      false
     end
   end
+
+  def tie
+       @@tie = @@board.all? do |x|
+        x.all? do |cell|
+           cell.class == String
+        end
+      end
+    p @@tie
+  end
+
+  def end_game
+    return true if @tie || winning_moves
+  end
+
 end
